9e4f431202cfb3006141c0f17c1f94db8000ffda
Konrad Makselan <makselan.konrad@gmail.com>
2021-02-26 03:31:54 +0100

    Add configuration for heroku

--- src/main/java/makselan/konrad/carexchange/CarExchangeApplication.java
+++ src/main/java/makselan/konrad/carexchange/CarExchangeApplication.java
@@ -3,9 +3,10 @@ package makselan.konrad.carexchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
-public class CarExchangeApplication {
+public class CarExchangeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarExchangeApplication.class, args);
